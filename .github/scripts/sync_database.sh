e5eab722e6d5951aec98850541cfc1155a7a8624
Kyle Taylor <kyletaylored@gmail.com>
2023-01-06 23:33:08 -0600

    Update workflow for db

--- .github/scripts/sync_database.sh
+++ .github/scripts/sync_database.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
 # Required variables
-# - BLUE_SITE
-# - GREEN_SITE
+# - BLUE_SITE_NAME
+# - BLUE_SITE_ENV
+# - GREEN_SITE_NAME
+# - GREEN_SITE_ENV
 
-terminus backup:create $BLUE_SITE --element=db --keep-for=1
-BLUE_SITE_DB_URL=$(terminus backup:get ${BLUE_SITE} --element=db)
-terminus import:database $GREEN_SITE $BLUE_SITE_DB_URL -y
+terminus backup:create $BLUE_SITE_NAME.$BLUE_SITE_ENV --element=db --keep-for=1
+BLUE_SITE_DB_URL=$(terminus backup:get $BLUE_SITE_NAME.$BLUE_SITE_ENV --element=db)
+terminus import:database $GREEN_SITE_NAME.$GREEN_SITE_ENV $BLUE_SITE_DB_URL -y
