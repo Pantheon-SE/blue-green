d81df19dc26a62e10d5c2212de37dd28c959297b
Kyle Taylor <kyle.taylor@getpantheon.com>
2023-01-07 10:29:25 -0600

    Debug code

--- .github/scripts/sync_code.sh
+++ .github/scripts/sync_code.sh
@@ -20,6 +20,10 @@ git config pull.rebase false
 BLUE_REPO=$(echo "ssh://codeserver.dev.$BLUE_SITE_NAME@codeserver.dev.$BLUE_SITE_NAME.drush.in:2222/~/repository.git")
 GREEN_REPO=$(echo "ssh://codeserver.dev.$GREEN_SITE_NAME@codeserver.dev.$GREEN_SITE_NAME.drush.in:2222/~/repository.git")
 
+# Make sure env are in git mode
+terminus connection:set $BLUE_SITE_NAME git
+terminus connection:set $GREEN_SITE_NAME git
+
 # Setup repo, add remote branch connection, sync code.
 git clone $GREEN_REPO $GREEN_SITE_NAME -b $BASE_BRANCH
 cd $GREEN_SITE_NAME
@@ -29,4 +33,5 @@ git remote add $BLUE_SITE_NAME $BLUE_REPO
 _check_branch=$(git ls-remote --heads $BLUE_SITE_NAME $BASE_BRANCH)
 [[ -n ${_check_branch} ]] && git pull $BLUE_SITE_NAME $BASE_BRANCH --rebase
 # Sync branch
+git pull origin $BASE_BRANCH -x theirs
 git push -u origin HEAD:refs/heads/$BASE_BRANCH
