d757dadb14ef4d57601a4f0818ffa0ecd4bc59be
Kyle Taylor <kyle.taylor@getpantheon.com>
2023-01-07 10:46:44 -0600

    Testing

--- .github/scripts/sync_code.sh
+++ .github/scripts/sync_code.sh
@@ -11,27 +11,27 @@ LNAME=$(terminus auth:whoami --field lastname)
 GIT_EMAIL=$(terminus auth:whoami --field email)
 GIT_NAME="$FNAME $LNAME"
 
-# Configure git defaults
-git config --global user.email "$GIT_EMAIL"
-git config --global user.name "$GIT_NAME"
-git config pull.rebase false
-
 # Fix github actions variables missing periods.
 BLUE_REPO=$(echo "ssh://codeserver.dev.$BLUE_SITE_NAME@codeserver.dev.$BLUE_SITE_NAME.drush.in:2222/~/repository.git")
 GREEN_REPO=$(echo "ssh://codeserver.dev.$GREEN_SITE_NAME@codeserver.dev.$GREEN_SITE_NAME.drush.in:2222/~/repository.git")
 
 # Make sure env are in git mode
-terminus connection:set $BLUE_SITE_NAME git
-terminus connection:set $GREEN_SITE_NAME git
+terminus connection:set $BLUE_SITE_NAME.dev git
+terminus connection:set $GREEN_SITE_NAME.dev git
 
 # Setup repo, add remote branch connection, sync code.
 git clone $GREEN_REPO $GREEN_SITE_NAME -b $BASE_BRANCH
 cd $GREEN_SITE_NAME
 git remote add $BLUE_SITE_NAME $BLUE_REPO
 
+# Configure git defaults
+git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$GIT_NAME"
+git config pull.rebase false
+
 # Check if remote branch exists
 _check_branch=$(git ls-remote --heads $BLUE_SITE_NAME $BASE_BRANCH)
 [[ -n ${_check_branch} ]] && git pull $BLUE_SITE_NAME $BASE_BRANCH --rebase
 # Sync branch
-git pull origin $BASE_BRANCH -x theirs
+git pull origin $BASE_BRANCH -X theirs --rebase
 git push -u origin HEAD:refs/heads/$BASE_BRANCH
