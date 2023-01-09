7bef6453eb7323599213d5687995bd3964154c89
Kyle Taylor <kyle.taylor@getpantheon.com>
2023-01-09 12:08:18 -0600

    debug terminus token

--- .github/scripts/setup-terminus.sh
+++ .github/scripts/setup-terminus.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo ln -s $GITHUB_WORKSPACE/.github/exe/terminus.phar /usr/local/bin/terminus
-terminus auth:login --machine-token="$pantheon-machine-token"
+#debug
+echo "terminus auth:login --machine-token=$PANTHEON_MACHINE_TOKEN"
+terminus auth:login --machine-token=$PANTHEON_MACHINE_TOKEN
