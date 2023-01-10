a59c3fd7a6d9e1566e5b8caf8954cb99f9e25d10
Kyle Taylor <kyle.taylor@getpantheon.com>
2023-01-09 22:49:46 -0600

    Add retries to file sync

--- .github/scripts/sync_files.sh
+++ .github/scripts/sync_files.sh
@@ -61,6 +61,6 @@ sudo sshfs \
 $BLUE_SITE_SFTP_PATH $MOUNT_PATH
 
 # Rclone
-rclone sync --transfers 25 $MOUNT_PATH $GREEN_SITE_ID
+rclone sync --progress --transfers 25 --retries 10 --retries-sleep 60s $MOUNT_PATH $GREEN_SITE_ID
 
 # No need to unmount, container will be reaped.
